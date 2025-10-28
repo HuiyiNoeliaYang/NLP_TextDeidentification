e4ecda5820755f7d0c3d25692e3e466a0dd6cb7b
Noelia <137973240+HuiyiNoeliaYang@users.noreply.github.com>
2025-10-28 15:04:25 -0400

    Initial commit

---
+++ load_model.py
@@ -0,0 +1,42 @@
+from datamodule import WikipediaDataModule
+import os
+from model import CoordinateAscentModel
+
+  
+  
+
+num_cpus = len(os.sched_getaffinity(0))
+
+  
+# chenage it later 
+checkpoint_path = "wikibio_roberta_roberta/model.ckpt"
+# The original path: "/content/unsupervised-deid/wikibio_roberta_roberta/model.ckpt"
+
+
+model = CoordinateAscentModel.load_from_checkpoint(checkpoint_path)
+
+dm = WikipediaDataModule(
+
+document_model_name_or_path=model.document_model_name_or_path,
+
+profile_model_name_or_path=model.profile_model_name_or_path,
+
+dataset_name='wiki_bio',
+
+dataset_train_split='train[:10%]',
+
+dataset_val_split='val[:20%]',
+
+dataset_version='1.2.0',
+
+num_workers=1,
+
+train_batch_size=64,
+
+eval_batch_size=64,
+
+max_seq_length=128,
+
+sample_spans=False,
+
+)
